0d9df566612623ea9b5f595115d7db17021e626a
WorrilessYi <148446839+WorrilessYi@users.noreply.github.com>
2024-03-18 01:58:48 +0000

    regrouping and rewriting

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf.csrf import CSRFProtect
 
-app=Flask(__name__)
-
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+csrf = CSRFProtect(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def welcome():
+    return render_template('welcome.html')
 
+@app.route('/information')
+def information():
+    return render_template('information.html')
+
+@app.route('/data_collection', methods=['GET', 'POST'])
+def data_collection():
+    return render_template('data_collection.html')
+
+
+# Define Flask Routes
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
